9c7094ae01b9d79ca078328e9b346bf59c1b9cc4
Oleksii <githubbai@gmail.com>
2023-04-04 19:05:46 +0300

    add nav

--- js/app.js
+++ js/app.js
@@ -9,3 +9,30 @@ box.addEventListener("animationend", AnimationHandler, false);
 function AnimationHandler () {
   box.classList.remove('ani_image');
 }
+
+
+
+
+const searchNav = document.querySelector('.navSymbol');
+
+searchNav.addEventListener('keyup', (event) => {
+    if(event.key === "C" || "c") {
+        window.location.href = '#symC';
+    };
+    if(event.key === "L" || "l") {
+        window.location.href = '#symL';
+    };
+    if(event.key === "S" || "s") {
+        window.location.href = '#symS';
+    };
+    if(event.key === "H" || "h") {
+        window.location.href = '#symH';
+    };
+    if(event.key === "W" || "w") {
+        window.location.href = '#symW';
+    };
+    if(event.key === "E" || "e") {
+        window.location.href = '#symE';
+    };
+});
+
