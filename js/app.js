e01c34cdef66a8b4cf46c355249e079453183449
Oleksii <githubbai@gmail.com>
2023-04-12 19:41:16 +0300

    render repos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 let box = document.querySelector('.boxAni');
 
+
+function renderRepos(myRepos) {
+  let htmlStr = myRepos.reduce(function (acc, repos) {
+    return acc + `<tr>
+        <td>${repos.full_name}</td>
+        <td><a href = "${repos.html_url}">${repos.html_url}</a></td>
+        <td><a>${repos.description || '---'}</a></td>
+    </tr>`;
+  });
+  document.querySelector('.table tbody').innerHTML = htmlStr;
+}
+
+
+
+let myRepos = [];
+
+fetch('https://api.github.com/users/OleksiiHub/repos').then(function (reposAll) {
+  return reposAll.json();
+}).then(function (reposAll) {
+  myRepos = reposAll;
+  renderRepos(myRepos);
+});
+
+
+
 box.addEventListener("click", function () {
   box.classList.add('ani_image');
 
@@ -26,4 +51,8 @@ document.addEventListener("keydown", (event) => {
     paragraphs[key].scrollIntoView({ block: "start", behavior: "smooth" });
   }
 });
+
+
+
+
 
